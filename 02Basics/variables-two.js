2be011952df940d186ad688bff412a9bdd682e16
sumit.jadiya <sumit.jadiya@appdirect.com>
2020-07-10 21:38:19 +0530

    interpolation

---
+++ 02Basics/variables-two.js
@@ -0,0 +1,16 @@
+const uid = "abc123"
+// uid = "abc2345" --> Not allowed
+
+var fullName = "Sumit Jadiya"
+var email = "jadiyaskj@gmail.com"
+var password = "1234567"
+var comparePassword = "1234567"
+var courseCount = 0
+var isLoggedInFromgoogle = false
+
+console.log("full name is " + fullName);
+console.log("email is ", email);
+console.log(`Password is ${password}`);
+console.log(`password again : ${comparePassword}`);
+console.log(`Courses count ${courseCount}`);
+console.log(`isLoggedInFromgoogle ${isLoggedInFromgoogle} `);
