6211759ff6fabefac73e0f80cfff7f4de2d17391
sumit.jadiya <sumit.jadiya@appdirect.com>
2020-07-17 16:20:46 +0530

    terniary

---
+++ 02Basics/07conditionalSignout.js
@@ -0,0 +1,6 @@
+// show user a signout button if he is authenticated
+// other wise show him option to login/signup
+
+var authenticated = true;
+
+authenticated ? console.log("show signout button") : console.log("show login option")
