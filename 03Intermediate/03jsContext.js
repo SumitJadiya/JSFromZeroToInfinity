e40596edb2e9275d9d8450ac61a08795f29886a3
sumit.jadiya <sumit.jadiya@appdirect.com>
2020-07-23 15:20:55 +0530

    context in js

---
+++ 03Intermediate/03jsContext.js
@@ -0,0 +1,12 @@
+sayHello()
+function sayHello() {
+    console.log("Hello")
+}
+
+
+if (2 === "2")
+    console.log("this is true")
+
+var myName = "Sumit"
+if (myname === window.myName)
+    console.log("again true")
--- README.md
+++ README.md
@@ -30,3 +30,4 @@ Javascript projects from basic to advanced on single repository.
 
 > Calling and referencing
 > Functions in variable
+> Context
