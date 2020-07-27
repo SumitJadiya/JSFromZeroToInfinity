32c9be68d088ea32877728f2cd3a20597f802ca8
sumit.jadiya <sumit.jadiya@appdirect.com>
2020-07-27 13:57:31 +0530

    this keyword

---
+++ 03Intermediate/06thisKeyword.js
@@ -0,0 +1,14 @@
+//TODO: part 1 for this keyword
+
+console.log(this); // access to global context
+
+var game = "basketball"
+
+/*
+function sayName() {
+    var name = "Sumit"
+    console.log(this);
+}
+
+sayName()
+*/
