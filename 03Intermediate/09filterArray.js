8f8a352c55fc85b7244bbe227cfa5e52cfdaf343
sumit.jadiya <sumit.jadiya@appdirect.com>
2020-07-31 19:47:34 +0530

    splice and slice

--- 03Intermediate/09filterArray.js
+++ 03Intermediate/09filterArray.js
@@ -6,3 +6,17 @@ console.log(testArray.fill(0))
 const myNumber = [23, 24, 25, 55, 66, 77, 88, 34, 56, 32, 98]
 const result = myNumber.filter(num => (num != 55))
 console.log(result)
+
+// Slice and Splice
+var users = ["Ted", "Tim", "Ton", "Sam", "Sor", "Sod"]
+
+console.log(users.slice(2, 5)) // [ 'Ton', 'Sam', 'Sor' ]
+console.log(users.slice(1)) // [ 'Tim', 'Ton', 'Sam', 'Sor', 'Sod' ]
+
+users.splice(1, 4, "Hi")
+console.log(users) // [ 'Ted', 'Hi', 'Sod' ]
+
+users = ["Ted", "Tim", "Ton", "Sam", "Sor", "Sod"]
+
+users.splice(1, 3, "Hi", "Bye")
+console.log(users) // [ 'Ted', 'Hi', 'Bye', 'Sor', 'Sod' ]
