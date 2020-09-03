32f98f51f6dad0690004afba84960108a2934563
sumit.jadiya <sumit.jadiya@appdirect.com>
2020-09-03 14:22:39 +0530

    more on class and object

--- 06LittleAdvanced/06classjs.js
+++ 06LittleAdvanced/06classjs.js
@@ -4,19 +4,26 @@ class User {
         this.email = email
     }
 
-    courseList = []
+    #courseList = [] // making courselist private
 
     getInfo() {
         return { name: this.name, email: this.email }
     }
 
     enrolledCourse(name) {
-        this.courseList.push(name)
+        this.#courseList.push(name)
     }
 
     getCourseList() {
-        return this.courseList
+        return this.#courseList
     }
 }
 
-module.exports = User 
+module.exports = User
+
+// more on objects
+const rock = new User("rock", "rock@rock.com")
+console.log(rock.getInfo())
+rock.enrolledCourse("React")
+console.log(rock.getCourseList())
+console.log(rock.courseList) // undefined
