f19571dbd13e5c313136dea029cead115208a932
sumit.jadiya <sumit.jadiya@appdirect.com>
2020-09-03 20:31:51 +0530

    q&a

--- 99OtherProjects/7-questions/setup/app.js
+++ 99OtherProjects/7-questions/setup/app.js
@@ -1,2 +1,32 @@
 //using selectors inside the element
+
+/*
+let questions = document.querySelectorAll(".question")
+
+questions.forEach(question => {
+
+    const btn = question.querySelector(".question-btn")
+
+    btn.addEventListener("click", () => {
+
+        questions.forEach(item => {
+            if (item !== question)
+                item.classList.remove("show-text")
+        })
+
+        question.classList.toggle("show-text")
+    })
+})
+*/
+
 // traversing the dom
+
+let questionBtns = document.querySelectorAll(".question-btn")
+
+questionBtns.forEach(questionBtn => {
+
+    questionBtn.addEventListener("click", e => {
+        const parentElement = e.currentTarget.parentElement.parentElement
+        parentElement.classList.toggle("show-text")
+    })
+})
