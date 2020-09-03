6f096b321a26c7cec6a3db24d01f4c45ffc5a68b
sumit.jadiya <sumit.jadiya@appdirect.com>
2020-09-03 10:13:27 +0530

    color flipper

--- 99OtherProjects/1-color-flipper/setup/app.js
+++ 99OtherProjects/1-color-flipper/setup/app.js
@@ -1,1 +1,16 @@
 const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+
+let color = document.querySelector(".color")
+let btnAction = document.querySelector(".btn-hero")
+
+document.body.style.backgroundColor = color.innerHTML
+
+btnAction.addEventListener("click", () => {
+    let random = getRandomNumber()
+    color.textContent = colors[random]
+    document.body.style.backgroundColor = colors[random]
+})
+
+const getRandomNumber = () => {
+    return Math.floor(Math.random() * 4)
+}
--- 99OtherProjects/1-color-flipper/setup/hex.js
+++ 99OtherProjects/1-color-flipper/setup/hex.js
@@ -1,1 +1,23 @@
+// generate random color code 
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+
+let color = document.querySelector(".color")
+let btn = document.querySelector(".btn-hero")
+
+document.body.style.backgroundColor = color.innerHTML
+
+btn.addEventListener("click", () => {
+
+    color.textContent = generateHexColor()
+    document.body.style.backgroundColor = generateHexColor()
+})
+
+const generateHexColor = () => {
+    let val = "#"
+
+    for (let index = 0; index < 6; index++) {
+        val += hex[Math.floor(Math.random() * hex.length)]
+    }
+
+    return val
+}
